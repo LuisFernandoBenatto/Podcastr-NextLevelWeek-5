b24dbde4d1b53b089c94b7f4ba07f3a6f87ba3ca
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2021-04-19 23:11:10 -0300

    Conceitos de React

---
+++ podcastr/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+
+function App() {
+  return (
+    <>
+      <h1>Hello, World!!!</h1>
+      <Button>Button 1</Button>
+      <Button>Button 2</Button>
+      <Button>Button 3</Button>
+      <Button>Button 4</Button>
+      <Button>Button 5</Button>
+      {/* <Button title="Button 1"/>
+      <Button title="Button 2"/>
+      <Button title="Button 3"/>
+      <Button title="Button 4"/>
+      <Button title="Button 5"/> */}
+    </>
+  );
+}
+
+export default App;
